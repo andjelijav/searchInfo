ec2d25f2cf6bccb959d5da3aa333b7fd300440b0
andjelijav <andjelijavelickovic@elfak.rs>
2023-12-18 14:38:59 +0100

    adding document collection in database

--- searchInfo/dataInfo/models.py
+++ searchInfo/dataInfo/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+class Documents(models.Model):
+    name=models.CharField(max_length=50)
+    extenesion=models.CharField(max_length=4)
+    create_date=models.DateTimeField(auto_now=True)
