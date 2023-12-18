72d264a76ff56875d3933bb56d754fd17f1dc83c
andjelijav <andjelijavelickovic@elfak.rs>
2023-12-18 12:28:04 +0100

    login form

--- searchInfo/dataInfo/forms.py
+++ searchInfo/dataInfo/forms.py
@@ -3,6 +3,7 @@ from django import forms
 #@breaf login form.
 #@param forms.Form > Base class for form.
 class LoginForm(forms.Form):
-    your_username = forms.CharField(label="Your username", max_length=100)
 
-    your_password=forms.PasswordInput()
+    your_username = forms.CharField(label="Your username", max_length=100,required=False)
+    
+    your_password=forms.CharField(label="Your password", max_length=50,required=False )
--- searchInfo/searchInfo/settings.py
+++ searchInfo/searchInfo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "dataInfo.apps.DatainfoConfig",
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
